ede057cc0b0fe4d0ee7cbaae25595ac2db6e687f
amdwildanabdillah <amd.wildanabdillah@gmail.com>
2026-03-02 02:43:33 +0700

    feat: Add .env file creation in build script for Vercel deployment

--- build.sh
+++ build.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+echo "🚀 Membuat file .env di server Vercel..."
+# Script ini akan membuat file .env dadakan saat proses build
+echo "SUPABASE_URL=$SUPABASE_URL" > .env
+echo "SUPABASE_ANON_KEY=$SUPABASE_ANON_KEY" >> .env
+echo "✅ File .env berhasil dibuat!"
+
 echo "🚀 Cek Flutter..."
 if cd flutter; then
   git pull && cd ..
@@ -8,9 +14,8 @@ else
 fi
 
 echo "✅ Flutter Siap. Mulai Build..."
-ls
-flutter/bin/flutter doctor
-flutter/bin/flutter clean
 flutter/bin/flutter config --enable-web
+flutter/bin/flutter clean
+flutter/bin/flutter pub get
 flutter/bin/flutter build web --release
 echo "🎉 Build Selesai!"
